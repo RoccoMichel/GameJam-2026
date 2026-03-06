240b6ab4919371d93f284416fe3d69a8d80f8eca
RoccoMichel <122682577+RoccoMichel@users.noreply.github.com>
2026-03-06 11:10:46 +0100

    Asset Imports & Player Heal

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,12 +3,21 @@ using UnityEngine;
 
 public class Player : Entity
 {
+    [Header("Player Healing")]
+    public float healRate = 1f;
+    public float timeUntilHeal = 6;
+    private float timer = 0;
     public override void Die()
     {
         if (immortal) print("Player died");
         else GameController.Instance.GameOver();
     }
-
+    private void Update()
+    {
+        // Heal Player when they haven't taken damage for some time
+        timer = Mathf.Clamp(timer += Time.deltaTime, 0, timeUntilHeal);
+        if (timer >= timeUntilHeal) Heal(healRate * Time.deltaTime);
+    }
     protected override void OnReset()
     {
         gameObject.tag = "Player";
@@ -18,6 +27,7 @@ public class Player : Entity
     public override void Damage(float amount)
     {
         base.Damage(amount);
+        timer = 0;
         StartCoroutine(FlashRed());
     }
 
