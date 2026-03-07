f40a1b0338fcba972171616808561a63f513360b
Rocco <122682577+RoccoMichel@users.noreply.github.com>
2026-03-07 13:58:02 +0100

    Bug Fixes
    - Glasses Refill station
    - Spawner Reference
    - Win Volume Reference
    - .abc build instantiate support
    - poster flip
    - better navmesh surface
    - more info on the itch page

--- Assets/Scripts/Effects/AlembicPlayer.cs
+++ Assets/Scripts/Effects/AlembicPlayer.cs
@@ -1,21 +1,51 @@
+using System.IO;
 using UnityEngine;
 using UnityEngine.Formats.Alembic.Importer;
 
 [RequireComponent(typeof(AlembicStreamPlayer))]
 public class AlembicPlayer : MonoBehaviour
 {
+    [Header("Importing .abc settings")]
+    [SerializeField] private string abcFileName;
+    [SerializeField] private Material material;
+    [SerializeField] private TimeValue startTime;
+    [SerializeField] private TimeValue endTime;
+    [SerializeField] private Vector3 scale = Vector3.one;
+    [SerializeField] private Vector3 offset;
+
+    [System.Serializable] public struct TimeValue
+    {
+        public bool @override;
+        public float newTime;
+    }
+
+    [Header("Animation Settings")]
     public bool loop = true;
     [Tooltip("If animation should reverse back to 0 when looping")]
     public bool pingPongLooping;
     public float speedModifier = 1;
     private AlembicStreamPlayer ABCplayer;
 
+    private void Awake()
+    {
+        ABCplayer = GetComponent<AlembicStreamPlayer>();
+
+        // fix alembic file reference in build because unity hates .abc files
+        if (string.IsNullOrEmpty(abcFileName)) return;
+        string path = Path.Combine(Application.streamingAssetsPath, abcFileName);
+        ABCplayer.LoadFromFile(path);
+
+        if (startTime.@override) ABCplayer.StartTime = startTime.newTime;
+        if (endTime.@override) ABCplayer.EndTime = endTime.newTime;
+
+        transform.position += offset;
+        transform.localScale = scale;
+        foreach (MeshRenderer mr in gameObject.GetComponentsInChildren<MeshRenderer>()) mr.material = material;
+    }
     private void Start()
     {
         if (speedModifier == 0) Debug.LogWarning("Speed Modifier is 0! No Animation will play.");
         if (pingPongLooping && !loop) Debug.LogWarning("Ping Pong Looping won't work if loop is false!");
-
-        ABCplayer = GetComponent<AlembicStreamPlayer>();
     }
 
     private void Update()
