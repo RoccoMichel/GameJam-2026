05d3b3e694ea41b79ce39dc68b90e43fdcfaec98
Rocco <122682577+RoccoMichel@users.noreply.github.com>
2026-07-04 15:13:06 +0200

    Made web version of waterFLYING and waterFLOOR

--- Assets/Scripts/Effects/FakeAlembicPlayer.cs
+++ Assets/Scripts/Effects/FakeAlembicPlayer.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 
+[RequireComponent(typeof(MeshRenderer), typeof(MeshFilter))]
 public class FakeAlembicPlayer : MonoBehaviour
 {
     public FakeAlembicSource source;
 
-    [SerializeField] private TimeValue startTime;
-    [SerializeField] private TimeValue endTime;
+    [SerializeField] private TimeValue startFrame;
+    [SerializeField] private TimeValue endFrame;
     [SerializeField] private Vector3 scale = Vector3.one;
     [SerializeField] private Vector3 offset;
 
@@ -13,7 +14,7 @@ public class FakeAlembicPlayer : MonoBehaviour
     public struct TimeValue
     {
         public bool @override;
-        public float newTime;
+        public float frame;
     }
 
     [Header("Animation Settings")]
@@ -23,5 +24,45 @@ public class FakeAlembicPlayer : MonoBehaviour
     public bool pingPongLooping;
     public float speedModifier = 1;
 
+    private MeshFilter meshFilter;
+    private float time;
 
+    private void Start()
+    {
+        if (source == null)
+        {
+            Debug.LogWarning($"FakeAlembicPlayer on {gameObject.name} is missing a source! All actions will fail.");
+            Debug.Break();
+        }
+
+        GetComponent<MeshRenderer>().material = source.material;
+        meshFilter = GetComponent<MeshFilter>();
+
+        transform.localScale = scale;
+        transform.localPosition += offset;
+
+        if (startFrame.@override) time = startFrame.frame;
+    }
+
+    private void Update()
+    {
+        if (Mathf.Floor(time) >= source.meshes.Length && !straightLooping && !pingPongLooping) return;
+
+        time += Time.deltaTime * source.frameRate * speedModifier;
+        int index = Mathf.FloorToInt(time);
+
+        if ((!endFrame.@override && Mathf.Floor(time) >= source.meshes.Length) || (!startFrame.@override && Mathf.Floor(time) < 0)
+            || (endFrame.@override && Mathf.Floor(time) >= endFrame.frame) || (startFrame.@override && Mathf.Floor(time) < startFrame.frame))
+        {
+            if (straightLooping && !startFrame.@override) time = 0;
+            else if (straightLooping && startFrame.@override) time = startFrame.frame;
+            else if (pingPongLooping)
+            {
+                time = Mathf.Clamp(index, 0, source.meshes.Length - 1);
+                speedModifier *= -1;
+            }
+        }
+
+        meshFilter.mesh = source.meshes[Mathf.Clamp(index, 0, source.meshes.Length - 1)];
+    }
 }
